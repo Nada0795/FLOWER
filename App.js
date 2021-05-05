639144ccf3a850405ed4b484663aa0811d2623b8
NADAMohammad <>
2021-05-05 15:49:57 +0000

    add JS file

---
+++ App.js
@@ -0,0 +1,32 @@
+
+//  message to show 
+alert ("This Is The Flowers Site")
+
+
+var userName = prompt('whats Your Name?')
+
+alert('Hello ' + userName)
+
+var flowerType = prompt("whats type of flowers you want ?")
+
+
+if(flowerType == 'DahliaIn')
+{img ='<img src ="img/m1.jpg">';}
+
+else if(flowerType == "Aster")
+{img = '<img src="img/m2.jpg">';}
+
+else if (flowerType == "Calla Lily")
+console.log ("this is beatiful flower")
+
+else {
+  alert("this type of flower not found")
+}
+
+// var theColor = prompt"what color do you want?"
+
+// if (theColor == "Red")
+// {theColor"Red";document.getElementsByName="#cf0000";}
+
+
+confirm("THANK YOU")
